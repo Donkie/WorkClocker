07151d1b531ba9c350a08e7e253750da426bdc8c
Daniel Hultgren <daniel.cf.hultgren@gmail.com>
2015-12-27 11:43:17 +0100

    Resources are now embedded into the exe!

--- WorkClocker/App.xaml.cs
+++ WorkClocker/App.xaml.cs
@@ -1,18 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using WorkClocker.Helpers;
 
 namespace WorkClocker
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        private static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            return EmbeddedAssembly.Get(args.Name);
+        }
+
+        [STAThread]
+        public static void Main()
+        {
+            const string res = "WorkClocker.References.MouseKeyboardActivityMonitor.dll";
+            EmbeddedAssembly.Load(res, "MouseKeyboardActivityMonitor.dll");
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
+            var application = new App();
+            application.InitializeComponent();
+            application.Run();
+        }
+
         private void Application_Exit(object sender, ExitEventArgs e)
         {
             WorkClocker.Properties.Settings.Default.Save();
---
+++ WorkClocker/Helpers/EmbeddedAssembly.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+
+namespace WorkClocker.Helpers
+{
+    /// <summary>
+    /// http://www.codeproject.com/Articles/528178/Load-DLL-From-Embedded-Resource
+    /// </summary>
+    public class EmbeddedAssembly
+    {
+        // Version 1.3
+
+        private static Dictionary<string, Assembly> _dic;
+
+        public static void Load(string embeddedResource, string fileName)
+        {
+            if (_dic == null)
+                _dic = new Dictionary<string, Assembly>();
+
+            byte[] ba;
+            Assembly asm;
+            var curAsm = Assembly.GetExecutingAssembly();
+
+            using (var stm = curAsm.GetManifestResourceStream(embeddedResource))
+            {
+                // Either the file is not existed or it is not mark as embedded resource
+                if (stm == null)
+                    throw new Exception(embeddedResource + " is not found in Embedded Resources.");
+
+                // Get byte[] from the file from embedded resource
+                ba = new byte[(int)stm.Length];
+                stm.Read(ba, 0, (int)stm.Length);
+                try
+                {
+                    asm = Assembly.Load(ba);
+
+                    // Add the assembly/dll into dictionary
+                    _dic.Add(asm.FullName, asm);
+                    return;
+                }
+                catch
+                {
+                    // Purposely do nothing
+                    // Unmanaged dll or assembly cannot be loaded directly from byte[]
+                    // Let the process fall through for next part
+                }
+            }
+
+            bool fileOk;
+            string tempFile;
+
+            using (var sha1 = new SHA1CryptoServiceProvider())
+            {
+                var fileHash = BitConverter.ToString(sha1.ComputeHash(ba)).Replace("-", string.Empty);
+
+                tempFile = Path.GetTempPath() + fileName;
+
+                if (File.Exists(tempFile))
+                {
+                    var bb = File.ReadAllBytes(tempFile);
+                    var fileHash2 = BitConverter.ToString(sha1.ComputeHash(bb)).Replace("-", string.Empty);
+
+                    fileOk = fileHash == fileHash2;
+                }
+                else
+                {
+                    fileOk = false;
+                }
+            }
+
+            if (!fileOk)
+            {
+                File.WriteAllBytes(tempFile, ba);
+            }
+
+            asm = Assembly.LoadFile(tempFile);
+
+            _dic.Add(asm.FullName, asm);
+        }
+
+        public static Assembly Get(string assemblyFullName)
+        {
+            if (_dic == null || _dic.Count == 0)
+                return null;
+
+            if (_dic.ContainsKey(assemblyFullName))
+                return _dic[assemblyFullName];
+
+            return null;
+        }
+    }
+}
